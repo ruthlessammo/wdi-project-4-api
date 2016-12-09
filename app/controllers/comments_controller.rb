61e529632dbea083d6d78667022f16e97f4685b3
William Hilton <williamjchilton@gmail.com>
2016-12-09 13:33:02 +0000

    comments security backend done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,26 +26,34 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1
   def update
-    if @comment.update(comment_params)
-      render json: @comment
+    if @track.user == current_user || !@track.user
+      if @comment.update(comment_params)
+        render json: @comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: { errors: ["Unauthorized"] }, status: 401
     end
   end
 
   # DELETE /comments/1
   def destroy
-    @comment.destroy
+    if @user == current_user
+      @comment.destroy
+    else
+      render json: { errors: ["Unauthorized"] }, status: 401
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def comment_params
-      params.require(:comment).permit(:body, :user_id, :track_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :track_id)
+  end
 end
